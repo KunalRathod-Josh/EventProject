1a1aee3414e9a0311aebc14855e8f24756dd8a6d
KunalRathod-Josh <kunal.rathod@joshsoftware.com>
2025-05-12 12:14:15 +0530

    rspec model added

--- factories/locations.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :location do
-    name { "Sample Location" }
-    address { "123 Sample St, Sample City, SC 12345" }
-    city { "Sample City" }
-    pin_code { "12345" }
-  end
-end
